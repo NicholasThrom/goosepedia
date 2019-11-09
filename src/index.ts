da2fc7c47ae5fb2e37a94e0eb364d2fc5523c7e4
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-11-09 15:05:30 -0500

    Use english wikipedia

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as http from "http";
 import fetch from "node-fetch";
 import { modifyText } from "./text-modifier";
 
-const baseURL = "https://wikipedia.org";
+const baseURL = "https://en.wikipedia.org";
 
 const port = parseInt(process.env.PORT || "3456", 10);
 
